929927a4ee80746f1561a65a41c0e8a016a04e9e
Raphael MICELI <raphael.miceli2025@campus-eni.fr>
2026-04-13 16:52:50 +0200

    init branch

---
+++ src/app/services/auth.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:8080/api/auth'; // L'URL de ton Spring Boot
+
+  constructor(private http: HttpClient) { }
+
+  register(userData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, userData);
+  }
+
+  login(credentials: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, credentials);
+  }
+}
