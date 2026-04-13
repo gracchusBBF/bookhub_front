736581fdf1eac365f7c966e5021de44bd8de3652
Clement PRAUD <clement.praud2025@campus-eni.fr>
2026-04-13 10:00:25 +0200

    CP - Generate components and pages for the app, config main routes in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Connexion } from './pages/connexion/connexion';
+import { DashboardAdmin } from './pages/dashboard-admin/dashboard-admin';
+import { DashboardUser } from './pages/dashboard-user/dashboard-user';
+import { DashboardLibrarian } from './pages/dashboard-librarian/dashboard-librarian';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: Home
+    },
+    {
+        path: 'connexion', component: Connexion
+    },
+    {
+        path: 'user', component: DashboardUser
+    },
+    {
+        path: 'librarian', component: DashboardLibrarian
+    },
+    {
+        path:  'admin', component: DashboardAdmin
+    }
+];
