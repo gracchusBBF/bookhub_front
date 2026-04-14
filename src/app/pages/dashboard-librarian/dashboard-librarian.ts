d5525c88f3623226a7f7d2efad189e49acfe50c4
Attila Simon <simon.attila.fr@gmail.com>
2026-04-14 12:14:23 +0200

    Angular MUI isntalled, librarian dashboard: basic elements

--- src/app/pages/dashboard-librarian/dashboard-librarian.ts
+++ src/app/pages/dashboard-librarian/dashboard-librarian.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+
+const ELEMENT_DATA = [
+  {
+    loan_id: '1',
+    userName: 'Jean Paul',
+    deadline: '20260602',
+    book: 'Rowling: Harry Potter',
+  },
+  {
+    loan_id: '2',
+    userName: 'Jean Paul2',
+    deadline: '20260604',
+    book: 'Jean-Paul Paul Jean: La vie à la bibliothèque',
+  },
+  {
+    loan_id: '3',
+    userName: 'Jean Paul3',
+    deadline: '20260601',
+    book: 'Paul-Jean Jean Paul: Le livre de ma vie',
+  },
+];
 
 @Component({
   selector: 'app-dashboard-librarian',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatTableModule],
   templateUrl: './dashboard-librarian.html',
   styleUrl: './dashboard-librarian.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardLibrarian {}
+export class DashboardLibrarian {
+  displayedColumns: string[] = ['userName', 'deadline', 'book'];
+  dataSource = ELEMENT_DATA;
+}
