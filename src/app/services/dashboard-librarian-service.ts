45ae4eb36ee8d41a5d938a9642c563ab3a8066eb
Attila Simon <simon.attila.fr@gmail.com>
2026-04-15 16:49:08 +0200

    Loan: Return a book OK - known bug: error when last book in the list returned

--- src/app/services/dashboard-librarian-service.ts
+++ src/app/services/dashboard-librarian-service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
+import { Observable } from 'rxjs';
 
 const protocol = 'http';
 const host = '127.0.0.1';
@@ -11,8 +12,27 @@ const baseURL = `${protocol}://${host}:${port}/api/loans`;
   providedIn: 'root',
 })
 export class DashboardLibrarianService {
-  private readonly http =  inject(HttpClient);
-  private token : string | null = localStorage.getItem("token");
+  private readonly http = inject(HttpClient);
+  private token: string | null = localStorage.getItem('token');
+
+  getLoansList() {
+    return this.http.get<[]>(baseURL, {
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+      },
+      responseType: 'json',
+    });
+  }
+
+  // api/loans/active
+  getActiveLoansList(): Observable<any[]> {
+    return this.http.get<any[]>(`${baseURL}/active`, {
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+      },
+      responseType: 'json',
+    });
+  }
 
   getOverdueLoans() {
     const url = `${baseURL}/overdue`;
@@ -22,5 +42,39 @@ export class DashboardLibrarianService {
       },
       responseType: 'json',
     });
+  }
+
+  getNbTotalLoans() {
+    const url = `${baseURL}/stats/totalLoans`;
+    return this.http.get<number>(url, {
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+      },
+      responseType: 'json',
+    });
+  }
+
+  getNbActiveLoans() {
+    const url = `${baseURL}/stats/activeLoans`;
+    return this.http.get<number>(url, {
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+      },
+      responseType: 'json',
+    });
+  }
+
+  returnABook(id: string) {
+    const url = `${baseURL}/${id}/return`;
+    return this.http.put(
+      url,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+        },
+        responseType: 'json',
+      },
+    );
   }
 }
