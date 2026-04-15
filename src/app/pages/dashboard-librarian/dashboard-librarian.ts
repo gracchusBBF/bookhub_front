45a54c70b2e8b097bf7db8d289ba948cf764e1fc
Attila Simon <simon.attila.fr@gmail.com>
2026-04-15 10:22:15 +0200

    Librarian dashboard: overdue OK

--- src/app/pages/dashboard-librarian/dashboard-librarian.ts
+++ src/app/pages/dashboard-librarian/dashboard-librarian.ts
@@ -4,10 +4,11 @@ import {MatCardModule} from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { DashboardLibrarianService } from '../../services/dashboard-librarian-service';
 import { AuthService } from '../../services/auth';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard-librarian',
-  imports: [MatCardModule, MatButtonModule, MatTableModule],
+  imports: [MatCardModule, MatButtonModule, MatTableModule, DatePipe],
   templateUrl: './dashboard-librarian.html',
   styleUrl: './dashboard-librarian.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -15,15 +16,15 @@ import { AuthService } from '../../services/auth';
 export class DashboardLibrarian implements OnInit {
   librarianStats = inject(DashboardLibrarianService);
   auth = inject(AuthService);
-  displayedColumns: string[] = ['userName', 'deadline', 'book'];
-  overdueLoans = signal<any[]>([]);
+  displayedColumns: string[] = ['userName', 'deadline', 'bookTitle'];
+  overdueLoans = signal<any>([]);
 
   ngOnInit() {
     this.librarianStats.getOverdueLoans().subscribe({
       next: (data) => {
         console.log('Overdue loans :');
         console.log(data);
-        // this.overdueLoans.set(data);
+        this.overdueLoans.set(data);
       },
       error: (err) => console.error(err),
     });
