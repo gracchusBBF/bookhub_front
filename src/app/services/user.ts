aebb788c4ae3a1a8be78cedb9f0407e5c07bb5d9
Raphael MICELI <raphael.miceli2025@campus-eni.fr>
2026-04-15 15:59:12 +0200

    user dash

--- src/app/services/user.ts
+++ src/app/services/user.ts
@@ -6,18 +6,35 @@ export interface DeleteAccountDTO {
   email: string;
   password: string;
 }
+export interface GetUserDataDTO {
+  email: string;
+  
+}
+export interface UserDataDTO {
+  email: string;
+  lastName: string;
+  firstName: string;
+  phoneNumber: string;
+  
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = 'http://localhost:8080/api/users/delete-account'; // Ajuste l'URL selon ton backend
+  private apiUrl = 'http://localhost:8080/api/users'; // Ajuste l'URL selon ton backend
 
   constructor(private http: HttpClient) {}
 
   deleteAccount(data : DeleteAccountDTO): Observable<any> {
-    return this.http.put(`${this.apiUrl}`, data, {
+    return this.http.put(`${this.apiUrl}`+"/delete-account", data, {
       responseType: 'text'
     });
+  }
+  getUser(email: string): Observable<UserDataDTO> {
+    return this.http.get<UserDataDTO>(`${this.apiUrl}`+`/details`+`/${email}`);
+  }
+  updateUser(email: string, data: Partial<UserDataDTO>): Observable<UserDataDTO> {
+    return this.http.put<UserDataDTO>(`${this.apiUrl}/update-details`, data);
   }
 }
