6d1a8415284197d499a83b00d96ddfe006264450
Attila Simon <simon.attila.fr@gmail.com>
2026-04-16 12:19:49 +0200

    Init Book management

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,24 +5,35 @@ import { DashboardAdmin } from './pages/dashboard-admin/dashboard-admin';
 import { DashboardUser } from './pages/dashboard-user/dashboard-user';
 import { DashboardLibrarian } from './pages/dashboard-librarian/dashboard-librarian';
 import {DeleteAccountFallback} from './pages/delete-account-fallback/delete-account-fallback'
+import { BookManagement } from './pages/book-management/book-management';
 
 export const routes: Routes = [
-    {
-        path: '', component: Home
-    },
-    {
-        path: 'connexion', component: Connexion
-    },
-    {
-        path: 'user', component: DashboardUser
-    },
-    {
-        path: 'librarian', component: DashboardLibrarian
-    },
-    {
-        path:  'admin', component: DashboardAdmin
-    },
-    {
-        path:  'deleted-account', component: DeleteAccountFallback
-    }
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'connexion',
+    component: Connexion,
+  },
+  {
+    path: 'user',
+    component: DashboardUser,
+  },
+  {
+    path: 'librarian',
+    component: DashboardLibrarian
+  },
+  {
+    path: 'librarian/books',
+    component: BookManagement,
+  },
+  {
+    path: 'admin',
+    component: DashboardAdmin,
+  },
+  {
+    path: 'deleted-account',
+    component: DeleteAccountFallback,
+  },
 ];
